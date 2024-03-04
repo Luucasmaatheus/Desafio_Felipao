873a6b49514d9b6afa207234ae481416b3257228
luucasmaatheus <lucascvc997@gmail.com>
2024-03-03 22:33:08 -0300

    Finalização do desafio2

--- desafio2.js
+++ desafio2.js
@@ -1,25 +1,29 @@
-
-
+// Chamada da função com valores de exemplo
 let saldo = calcularPartidasRankeadas(100,45);
 console.log(`O Herói tem o de saldo ${saldo} está no nível de ${nivel}`)
 
+
+// Função para calcular o saldo de partidas ranqueadas
 function calcularPartidasRankeadas(vitorias, derrotas){
     let saldo = vitorias - derrotas;
-    
-    if (saldo <= 10){
-        nivel = "Ferro";
-    } else if (saldo >= 11 && saldo <= 20){
-        nivel = "Bronze";
-    } else if (saldo >= 21 && saldo <= 50){
-        nivel = "Prata";
-    } else if (saldo >= 51 && saldo <= 80){
-        nivel = "Ouro";
-    } else if (saldo >= 81 && saldo <= 90){
-        nivel = "Diamante";
-    } else if (saldo >= 91 && saldo <= 100){
-        nivel = "Lendário";
-    } else if (saldo >= 101){
-        nivel = "Imortal";
-    }
-    return saldo
+
+// Determina o nível com base no saldo
+    saldo;
+if (saldo <= 10){
+    nivel = "Ferro";
+} else if (saldo >= 11 && saldo <= 20){
+    nivel = "Bronze";
+} else if (saldo >= 21 && saldo <= 50){
+    nivel = "Prata";
+} else if (saldo >= 51 && saldo <= 80){
+    nivel = "Ouro";
+} else if (saldo >= 81 && saldo <= 90){
+    nivel = "Diamante";
+} else if (saldo >= 91 && saldo <= 100){
+    nivel = "Lendário";
+} else if (saldo >= 101){
+    nivel = "Imortal";
+}
+// Retorna o saldo calculado
+return saldo
 }
