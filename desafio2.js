1787bb79dfbbc4868de724daaf7455aa0fc0d111
luucasmaatheus <lucascvc997@gmail.com>
2024-03-03 22:07:47 -0300

    meu segundo desafio

---
+++ desafio2.js
@@ -0,0 +1,25 @@
+
+
+let saldo = calcularPartidasRankeadas(100,45);
+console.log(`O Herói tem o de saldo ${saldo} está no nível de ${nivel}`)
+
+function calcularPartidasRankeadas(vitorias, derrotas){
+    let saldo = vitorias - derrotas;
+    
+    if (saldo <= 10){
+        nivel = "Ferro";
+    } else if (saldo >= 11 && saldo <= 20){
+        nivel = "Bronze";
+    } else if (saldo >= 21 && saldo <= 50){
+        nivel = "Prata";
+    } else if (saldo >= 51 && saldo <= 80){
+        nivel = "Ouro";
+    } else if (saldo >= 81 && saldo <= 90){
+        nivel = "Diamante";
+    } else if (saldo >= 91 && saldo <= 100){
+        nivel = "Lendário";
+    } else if (saldo >= 101){
+        nivel = "Imortal";
+    }
+    return saldo
+}
